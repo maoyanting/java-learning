40e0fade72fd09c9bbe1bdd66944522ebb947ea8
maoyanting <maoyanting@souche.com>
2019-07-31 16:49:30 +0800

    add 整型的缓存

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Java 练习
 
+![language](https://img.shields.io/badge/language-Java-blue.svg)
+
--- src/main/java/com/sandao/javalearning/basic/datatype/Integer0.java
+++ src/main/java/com/sandao/javalearning/basic/datatype/Integer0.java
@@ -9,6 +9,7 @@ package com.sandao.javalearning.basic.datatype;
 public class Integer0 {
     public static void main(String[] args) {
         testNumberType();
+        testCache();
     }
 
     /**
@@ -19,5 +20,22 @@ public class Integer0 {
         System.out.println(a == b);
         System.out.println(b == c);
         System.out.println(a == c);
+    }
+
+    /**
+     * 缓存适用于整数值区间-128 至 +127。
+     * Byte, Short, Long 同理
+     * 此处使用了自动装箱，实际上为调用 Integer.valueOf(125)
+     * 缓存的主要原因在于 valueOf 方法中的 IntegerCache 内部类
+     */
+    private static void testCache(){
+        Integer a = 125;
+        Integer b = 125;
+        Integer c = 129;
+        Integer d = 129;
+        System.out.print("两个整型 125 对比：");
+        System.out.println(a == b);
+        System.out.print("两个整型 129 对比：");
+        System.out.println(d == c);
     }
 }
